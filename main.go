ba861a02ccce7725a972b657fa789a6135319a3c
JalanJiang <448300947@qq.com>
2020-10-16 17:45:49 +0800

    feat(main): 初始化启动文件

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"JalanJiang/douban-tool-server/pkg/setting"
+	"JalanJiang/douban-tool-server/routers"
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	// 初始化路由
+	router := routers.InitRouter()
+
+	// 服务器设置
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	s.ListenAndServe()
 }
