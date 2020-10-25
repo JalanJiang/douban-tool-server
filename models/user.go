32bb7e65c7fa0df5a9060154bc2f077763c815d5
JalanJiang <448300947@qq.com>
2020-10-25 15:08:58 +0800

    refactor(model): 修改字段

--- models/user.go
+++ models/user.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 // User Model definition
 type User struct {
 	gorm.Model
-	Openid     string `gorm:"index"`
-	Unionid    string `gorm:"index"`
+	OpenID     string `gorm:"index"`
+	UnionID    string `gorm:"index"`
 	SessionKey string
 }
